3797b83cdddbb1f8afa916cf20de20c134f52f2c
oksahil <oksana.khylyk@gmail.com>
2023-02-28 20:07:29 +0100

    start

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/goit-react-hw-06-phonebook-redux-slice">
+        <BrowserRouter basename="/goit-react-hw-07-phonebook">
           <App />
         </BrowserRouter>
       </PersistGate>
